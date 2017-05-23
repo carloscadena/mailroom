f7b97276f22749b2b938df51d3a32ae7b14fe210
yanil3500 <yanil3500@gmail.com>
2017-05-22 20:40:09 -0700

    adds a lot more tests and test cases; all tests pass for python 2.7 and python 3.6

--- src/mailroom.py
+++ src/mailroom.py
@@ -7,21 +7,24 @@ donor_list = {
     'Carlos': [232, 32.34, 252],
     'Elyanil': [234.23, 2342, 2335],
     'Ely': [23.50, 255],
-    'Anna': []
+    'Anna': [0.00, 0, 0.00]
 }
 
 PROMPTS = {
     'thank_or_report': """\n
     Would you like to send a 'Thank you Email' or 'Create a Report'?
 
-    Select 1 for 'Thank you Email' or 2 for 'Create a Report' ('q' to exit.):""",
+    Select 1 for 'Thank you Email' or 2 for 'Create a Report'
+    ('q' to exit.):""",
     'send_thank_you': """\n
-    Enter the donor's full name or to see a list of donors type 'list' ('q' to restart): """,
+    Enter the donor's full name or to see a list of donors type 'list'
+    ('q' to restart): """,
     'ask_for_a_donation_amount': """Enter donation amount: """,
 
     }
 PROMPTS_WRONG_INPUT = {
-    'thank_or_report': 'Invalid selection. Please select either 1 or 2. (\'q\' to exit.)',
+    'thank_or_report': """Invalid selection. Please select either 1 or 2.
+    (q to exit.)""",
     'ask_for_a_donation_amount': 'Enter a (positive) number. Please: '
 
 }
@@ -39,50 +42,54 @@ def thank_or_report(prompt=PROMPTS['thank_or_report']):
         thank_or_report(PROMPTS_WRONG_INPUT['thank_or_report'])
 
 
-# def is_valid_selection(selection):
-#     switcher = {
-#         '1': send_thank_you,
-#         '2': create_report,
-#         'q': sys.exit
-#     }
-#     return switcher.get(selection, thank_or_report(PROMPTS_WRONG_INPUT['thank_or_report']))
-
-
 def send_thank_you():  # pragma: no cover
     user_input = input_handler(PROMPTS['send_thank_you'])
     if user_input == 'list':
+        if '------->[ Donors |\n' not in print_donor_names(donor_list):
+            send_thank_you()
         print(print_donor_names(donor_list))
     elif user_input == 'q':
         thank_or_report()
     elif user_input in donor_list:
         ask_for_a_donation_amount(user_input)
     else:
-        add_name_to_donor_list(user_input)
+        if add_name_to_donor_list(user_input):
+            ask_for_a_donation_amount(user_input)
 
 
 def print_donor_names(donor_list):
     """
     print donor names to command line
     """
-    roster = """------->[ Donors |\n"""
-    for donor in donor_list.keys():
-        roster += """------->[ {} |\n""".format(donor)
-    return roster
+    if not isinstance(donor_list, dict):
+        return 'Input value is not a dictionary.'
+    if len(donor_list.keys()) > 0:
+        roster = """------->[ Donors |\n"""
+        for donor in donor_list.keys():
+            roster += """------->[ {} |\n""".format(donor)
+        return roster
+    else:
+        return 'The donor list is empty.'
 
 
-def add_name_to_donor_list(name): # pragma: no cover
+def add_name_to_donor_list(name):
     """
     adds new donor to list of donors
     """
-    donor_list[name] = []
-    ask_for_a_donation_amount(name)
+
+    donor_list[name] = [0.00, 0, 0.00]
+
+    # returns 'True' to indicate that the name has been added to the list
+    return True
+
 
 
 def create_email(name, amount):
     """
     create email thanking the donor
     """
-    return '''
+    if isinstance(name, str) and isinstance(amount, float) or isinstance(amount, int):
+        return '''
     Dear {},
 
     Thank you so much for your donation of ${:.2f} dollars!
@@ -91,6 +98,8 @@ def create_email(name, amount):
 
         The Establishment
      '''.format(name, amount)
+
+    return """One of the input values provided is invalid."""
 
 
 def ask_for_a_donation_amount(name, prompt=PROMPTS['ask_for_a_donation_amount']):  # pragma: no cover
@@ -108,14 +117,18 @@ def checks_donation(name, amount):  # pragma: no cover
     if is_valid_donation(amount):
         add_donation(name, amount)
         send_email(name, float(amount))
+        return True
     else:
         ask_for_a_donation_amount(name, PROMPTS_WRONG_INPUT['ask_for_a_donation_amount'])
+        return False
 
 
 def add_donation(name, amount):
     """
     add donation to donation_list
     """
+    if name not in donor_list:
+        add_name_to_donor_list(name)
     donor_list[name].append(float(amount))
     return True
 
@@ -131,7 +144,7 @@ def is_valid_donation(amount):
         return False
 
 
-def input_handler(prompt): #pragma: no cover
+def input_handler(prompt):  # pragma: no cover
     """
     responsible for handling input
     """
@@ -139,16 +152,21 @@ def input_handler(prompt): #pragma: no cover
     return user_response
 
 
-def create_report(donor_list): #pragma: no cover
+def create_report(donor_list):
     """
     creates the report
     """
-    return prepare_donor_list_for_report(donor_list)
+    calculations = prepare_donor_list_for_report(donor_list)
+
+    sorted_calculations = sort_by_donation_amount(calculations)
+
+    table = creates_table_for_report(sorted_calculations)
+    return table
 
 
-def prepare_donor_list_for_report(a_donor_list): # pragma: no cover
+def prepare_donor_list_for_report(a_donor_list):
     """
-    Calculates total amount contributions, number of contributions, 
+    Calculates total amount contributions, number of contributions,
     and avg. donation amount for each donor
     """
     contributions_calculations = {}
@@ -164,22 +182,22 @@ def prepare_donor_list_for_report(a_donor_list): # pragma: no cover
                 len(a_donor_list[donor]),
                 sum(a_donor_list[donor])]
 
-    return sort_by_donation_amount(contributions_calculations)
+    return contributions_calculations
 
 
-def sort_by_donation_amount(contributions): # pragma: no cover
+def sort_by_donation_amount(contributions):
     contributions = sorted(
         contributions.items(),
         key=lambda x: x[1], reverse=True)
-    return creates_table_for_report(contributions)
+    return contributions
 
 
-def creates_table_for_report(contributions): # pragma: no cover
-    table = ('='*10).join([' Name ', ' Total Amount ', ' # of Contributions ', ' Avg. Donation Amount'])
+def creates_table_for_report(contributions):
+    table = ('='*10).join(
+        [' Name ', ' Total Amount ', ' # of Contributions ', ' Avg. Donation Amount'])
     table += '\n'
     table += ('-'*len(table))
     table += '\n'
-
 
     for donor in contributions:
         if len(str(donor[1][0])) > 6:
@@ -200,20 +218,15 @@ def creates_table_for_report(contributions): # pragma: no cover
             table += '{00:25}'.format((donor[1][1]))
             table += '{0:25}'.format('')
             table += '${00:.2f}'.format((donor[1][2]))
-
-        (donor[1][1], donor[1][2])
-        # table += (' '*10).join(('{} ${:.2f} ${:.2f} ${:.2f}'.format(donor[0], donor[1][0], donor[1][1], donor[1][2])).split())
         table += '\n'
-
     return table
 
 
-
-def send_email(name, amount): # pragma: no cover
+def send_email(name, amount):  # pragma: no cover
     """
     prints the email to the console
     """
-    
+
     print(create_email(name, sum(donor_list[name])))
     thank_or_report()
 
